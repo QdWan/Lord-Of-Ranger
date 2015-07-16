98e5d7074fb07d8b0952a2344429352d5efd49ae
akina <testakina@gmail.com>
2015-07-16 19:27:28 +0900

    typoの修正

--- LordOfRanger/Options/OptionsForm.Designer.cs
+++ LordOfRanger/Options/OptionsForm.Designer.cs
@@ -499,9 +499,9 @@
 			this.label8.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.label8.Location = new System.Drawing.Point(7, 20);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(248, 18);
+			this.label8.Size = new System.Drawing.Size(246, 18);
 			this.label8.TabIndex = 0;
-			this.label8.Text = "Load Of Ranger Enable/Disable Switching";
+			this.label8.Text = "Lord Of Ranger Enable/Disable Switching";
 			// 
 			// groupBox3
 			// 
