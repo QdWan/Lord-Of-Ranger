722a1e0bb0bdd5228f68e2e2efeae1c8634ac8e7
akina <testakina@gmail.com>
2016-03-01 22:48:29 +0900

    MouseのSetもCommonに移動

--- LordOfRanger/Mouse/Common.cs
+++ LordOfRanger/Mouse/Common.cs
@@ -26,4 +26,21 @@ namespace LordOfRanger.Mouse {
 		}
 
 	}
+
+	class Set {
+		internal readonly Operation op;
+		internal readonly int x;
+		internal readonly int y;
+		internal readonly int sleepBetween;
+		internal readonly int sleepAfter;
+
+		internal Set( Operation op, int x, int y, int sleepBetween, int sleepAfter ) {
+			this.op = op;
+			this.x = x;
+			this.y = y;
+			this.sleepBetween = sleepBetween;
+			this.sleepAfter = sleepAfter;
+		}
+
+	}
 }
